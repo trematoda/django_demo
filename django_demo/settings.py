12cbad41b487d8c4efca57c5d7da629b209fd17f
Danny DeLuca <danny.deluca@medicine.ufl.edu>
2018-06-27 11:06:19 -0400

    Create and run initial migration

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'django_demo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.demo'),
     }
 }
 
