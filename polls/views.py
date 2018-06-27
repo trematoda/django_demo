96635578b58f975ed6069ba70d73f308626864f9
Danny DeLuca <danny.deluca@medicine.ufl.edu>
2018-06-27 14:20:32 -0400

    Add new routes

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('I''m a teenage lobotomy!')
+
+
+def detail(request, question_id):
+    response = "Detail of question %s"
+    return HttpResponse(response % question_id)
+
+
+def results(request, question_id):
+    response = "Results of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    response = "Register a vote on question %s"
+    return HttpResponse(response % question_id)
